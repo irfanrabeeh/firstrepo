51ec32d4459e5cab64bd9ecef3f887ce344d14e2
Irfan <Irfan@IRFAN>
2019-09-23 15:14:42 +0530

    Commiting first Time

---
+++ GitHub/src/com/test/Main.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData obj=new SalesData();
+		System.out.println("Hello UST");
+		obj.display();
+		displayGreetings();
+
+	}
+	
+	private static void displayGreetings()
+	{
+		System.out.println("Welcome to the world");
+	}
+
+}
